e87bf1a8fbcf7382771adb24a8df9f38c0485684
Jakub Cieplinski <jakub.cieplinski@outlook.com>
2024-03-11 15:28:48 +0100

    Refactored and added tests For AssetManager

--- tests/ut/manager/AssetManagerTests.cpp
+++ tests/ut/manager/AssetManagerTests.cpp
@@ -7,23 +7,46 @@
 namespace roen
 {
 
+namespace
+{
+const std::string FALSE_PATH {"FALSE"};
+const std::string DUMMY_PATH {"DUMMY_PATH"};
+const std::string DUMMY_ID {"DUMMY_ID"};
+const std::string DUMMY_PATH_2 {"DUMMY_PATH_2"};
+const std::string DUMMY_ID_2 {"DUMMY_ID_2"};
+constexpr std::uint64_t DUMMY_HASH{123};
+
+constexpr std::uint32_t VAL_FALSE{0};
+constexpr std::uint32_t VAL_1{1};
+constexpr std::uint32_t VAL_2{2};
+
+const std::map<std::string, int> pathToVal = {
+        {FALSE_PATH, VAL_FALSE},
+        {DUMMY_PATH, VAL_1},
+        {DUMMY_PATH_2, VAL_2}
+};
+
+}
+
 namespace interfaces
 {
 
-template <bool val>
-class StubAsset : public Asset<bool>
+template <int val>
+class StubAsset : public Asset<int>
 {
 public:
     bool loadAsset(const std::string& path) override
     {
-        return val;
+        asset_ = pathToVal.at(path);
+
+        return asset_;
     }
-    [[nodiscard]] bool get() const override
+    [[nodiscard]] int get() const override
     {
         return asset_;
     }
 private:
-    bool asset_{val};
+    int asset_{val};
 };
 
 } // interfaces
@@ -31,41 +54,43 @@ private:
 namespace manager
 {
 
-namespace
-{
-const std::string DUMMY_PATH {"DUMMY_PATH"};
-const std::string DUMMY_ID {"DUMMY_ID"};
-constexpr std::uint64_t DUMMY_HASH{123};
-}
-
 class AssetManagerTests : public ::testing::TestWithParam<std::pair<bool, bool>>
 {
 };
 
-TEST_F(AssetManagerTests, ShouldThrowOnUnsucessfullAssetLoad)
+TEST_F(AssetManagerTests, loadAsset_ShouldLoadAsset)
 {
-    auto sut = AssetManager<interfaces::StubAsset<false>>();
-    EXPECT_THROW(sut.loadAsset(DUMMY_ID, DUMMY_PATH), std::runtime_error);
-}
-
-TEST_F(AssetManagerTests, FreeShouldUnloadAssets)
-{
-    auto sut = AssetManager<interfaces::StubAsset<true>>();
+    auto sut = AssetManager<interfaces::StubAsset<VAL_1>>();
     EXPECT_NO_THROW(sut.loadAsset(DUMMY_ID, DUMMY_PATH));
-    EXPECT_TRUE(sut.getAsset(roen::hashString(DUMMY_ID)));
+    EXPECT_EQ(sut.getAsset(roen::hashString(DUMMY_ID)), VAL_1);
 }
 
-TEST_F(AssetManagerTests, ShouldThrowOnAccessingUnloadedAsset)
+TEST_F(AssetManagerTests, loadAsset_ShouldThrowOnUnsucessfullAssetLoad)
 {
-    auto sut = AssetManager<interfaces::StubAsset<true>>();
+    auto sut = AssetManager<interfaces::StubAsset<VAL_FALSE>>();
+    EXPECT_THROW(sut.loadAsset(DUMMY_ID, FALSE_PATH), std::runtime_error);
+}
+
+TEST_F(AssetManagerTests, free_ShouldUnloadAssets)
+{
+    auto sut = AssetManager<interfaces::StubAsset<VAL_1>>();
+    EXPECT_NO_THROW(sut.freeAssets());
     EXPECT_THROW(sut.getAsset(DUMMY_HASH), std::out_of_range);
 }
 
-TEST_F(AssetManagerTests, ShouldSuccesfullyStoreAndReturnLoadedAsset)
+TEST_F(AssetManagerTests, getAsset_ShouldThrowOnAccessingNotPresentAsset)
 {
-    auto sut = AssetManager<interfaces::StubAsset<true>>();
+    auto sut = AssetManager<interfaces::StubAsset<VAL_1>>();
+    EXPECT_THROW(sut.getAsset(DUMMY_HASH), std::out_of_range);
+}
+
+TEST_F(AssetManagerTests, getAsset_ShouldReturnCorrectAsset)
+{
+    auto sut = AssetManager<interfaces::StubAsset<VAL_1>>();
     EXPECT_NO_THROW(sut.loadAsset(DUMMY_ID, DUMMY_PATH));
-    EXPECT_TRUE(sut.getAsset(roen::hashString(DUMMY_ID)));
+    EXPECT_NO_THROW(sut.loadAsset(DUMMY_ID_2, DUMMY_PATH_2));
+    EXPECT_EQ(sut.getAsset(roen::hashString(DUMMY_ID)), VAL_1);
+    EXPECT_EQ(sut.getAsset(roen::hashString(DUMMY_ID_2)), VAL_2);
 }
 
 } // manager
