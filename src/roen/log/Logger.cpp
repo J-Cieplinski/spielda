89ca1a7969dd1169733c1952d32a519d2469cd27
Jakub Cieplinski <jakub.cieplinski@outlook.com>
2024-03-16 09:36:27 +0100

    Added throw messages to Logger to easier detect when it is uninitialized

--- src/roen/log/Logger.cpp
+++ src/roen/log/Logger.cpp
@@ -17,21 +17,37 @@ void Logger::Init()
 
 void Logger::setSdkLogLevel(spdlog::level::level_enum level)
 {
+    if(!sdkLogger_)
+    {
+        throw std::runtime_error("SDK LOGGER was not initalized");
+    }
     sdkLogger_->set_level(level);
 }
 
 void Logger::setAppLogLevel(spdlog::level::level_enum level)
 {
+    if(!appLogger_)
+    {
+        throw std::runtime_error("APP LOGGER was not initalized");
+    }
     appLogger_->set_level(level);
 }
 
 std::shared_ptr<spdlog::logger>& Logger::getSdkLogger()
 {
+    if(!sdkLogger_)
+    {
+        throw std::runtime_error("SDK LOGGER was not initalized");
+    }
     return sdkLogger_;
 };
 
 std::shared_ptr<spdlog::logger>& Logger::getAppLogger()
 {
+    if(!appLogger_)
+    {
+        throw std::runtime_error("APP LOGGER was not initalized");
+    }
     return appLogger_;
 };
 
