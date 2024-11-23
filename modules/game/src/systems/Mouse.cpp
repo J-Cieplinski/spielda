f680313a0601e4fc913a5657c782c776007f133d
Jakub Cieplinski <jakub.cieplinski@outlook.com>
2024-11-23 18:30:47 +0100

    Added mouse position translation to real screen coords

--- modules/game/src/systems/Mouse.cpp
+++ modules/game/src/systems/Mouse.cpp
@@ -1,5 +1,6 @@
 #include <systems/Mouse.hpp>
 
+#include <CoreConfig.hpp>
 #include <events/Mouse.hpp>
 
 #include <raylib.h>
@@ -19,24 +20,37 @@ void Mouse::update()
     if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
     {
         eventDispatcher_.trigger(events::Mouse {
-            .position = GetMousePosition(),
+            .position = getVirtualMousePosition(),
             .mouseButton = MOUSE_BUTTON_LEFT
         });
     }
     else if(IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
     {
         eventDispatcher_.trigger(events::Mouse {
-            .position = GetMousePosition(),
+            .position = getVirtualMousePosition(),
             .mouseButton = MOUSE_BUTTON_RIGHT
         });
     }
     else if(!Vector2Equals(GetMouseDelta(), Vector2Zero()))
     {
         eventDispatcher_.trigger(events::Mouse {
-            .position = GetMousePosition(),
+            .position = getVirtualMousePosition(),
             .mouseButton = -1
         });
     }
+}
+
+Vector2 Mouse::getVirtualMousePosition() const
+{
+    const float scale {std::min(static_cast<float>(GetScreenWidth())/RENDER_WIDTH, static_cast<float>(GetScreenHeight())/RENDER_HEIGHT)};
+
+    const auto [mouseX, mouseY] = GetMousePosition();
+    const Vector2 virtualMousePosition {
+        .x = (mouseX - (static_cast<float>(GetScreenWidth()) - (RENDER_WIDTH * scale)) * 0.5f) / scale,
+        .y = (mouseY - (static_cast<float>(GetScreenHeight()) - (RENDER_HEIGHT * scale)) * 0.5f) / scale,
+    };
+
+    return Vector2Clamp(virtualMousePosition, Vector2Zero(), Vector2{ RENDER_WIDTH, RENDER_HEIGHT });
 }
 
 } // spielda::system
